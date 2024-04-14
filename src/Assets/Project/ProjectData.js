8d1b059bc5e6e0b5d9736944b04d21cc55a5f148
vaibhav Pandey <vaibhav.pandey0798@gmail.com>
2024-04-15 00:11:51 +0530

    Create ProjectData.js

---
+++ src/Assets/Project/ProjectData.js
@@ -0,0 +1,52 @@
+const projectData = [
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+    {
+        name: "Project",
+        description: "This is a project",
+        image: "",
+        link: ""
+    },
+]
+
+export default projectData
