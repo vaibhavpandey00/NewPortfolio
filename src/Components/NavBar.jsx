9d860325bd0a902bc44d7f2a53b81e6f230675ae
vaibhav Pandey <vaibhav.pandey0798@gmail.com>
2024-04-16 17:29:51 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -14,17 +14,32 @@ const NavBar = () => {
     }, [])
 
     return (
-        <header
-            data-aos="fade-down"
-            className="flex justify-between items-center fixed w-screen px-2 py-2 h-15 md:px-7 md:h-20 z-50 bg-primary">
-            <h1 className="text-5xl font-logo mt-3 md:mt-4">Vaibhav</h1>
+        <header className="flex justify-between items-center fixed w-screen px-2 py-2 h-15 md:px-7 md:h-20 z-50 bg-primary">
+            <h1
+                data-aos="fade-right"
+                className="text-5xl font-logo mt-3 md:mt-4">Vaibhav</h1>
 
             <ul className="hidden md:flex justify-center items-center gap-6">
-                <li className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='home' smooth duration={800}>Home</Link></li>
-                <li className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='about' smooth duration={800}>About</Link></li>
-                <li className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='project' smooth duration={800}>Project</Link></li>
-                <li className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='experience' smooth duration={800}>Experience</Link></li>
-                <li className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='contact' smooth duration={800}>Contact</Link></li>
+                <li
+                    data-aos="fade-down"
+                    data-aos-delay="200"
+                    className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='home' smooth duration={800}>Home</Link></li>
+                <li
+                    data-aos="fade-down"
+                    data-aos-delay="300"
+                    className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='about' smooth duration={800}>About</Link></li>
+                <li
+                    data-aos="fade-down"
+                    data-aos-delay="400"
+                    className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='project' smooth duration={800}>Project</Link></li>
+                <li
+                    data-aos="fade-down"
+                    data-aos-delay="500"
+                    className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='experience' smooth duration={800}>Experience</Link></li>
+                <li
+                    data-aos="fade-down"
+                    data-aos-delay="600"
+                    className="cursor-pointer p-3 rounded-3xl text-xl font-medium hover:font-semibold hover:scale-110 duration-300"><Link to='contact' smooth duration={800}>Contact</Link></li>
             </ul>
 
             <div onClick={() => setNav(!nav)} className="cursor-pointer z-10 text-gray-500 md:hidden">
